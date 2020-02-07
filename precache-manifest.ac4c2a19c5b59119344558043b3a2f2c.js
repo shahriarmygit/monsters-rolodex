245fd9daf8eba91ee38f1871e10093aa216ed4c1
shahriarmygit <shahriarbfg@gmail.com>
2020-02-07 20:59:22 +0600

    Updates

---
+++ precache-manifest.ac4c2a19c5b59119344558043b3a2f2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80ed00577a5d276ad73f56ed4b26915",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3c4bb3ccd51863b45069",
+    "url": "/monsters-rolodex/static/css/main.ea09c1fa.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c4bb3ccd51863b45069",
+    "url": "/monsters-rolodex/static/js/main.374356f1.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
